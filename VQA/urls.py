bcb1e5bb254d8381c93157d60715108a73a53a37
praveen <prav20ad036@rmkcet.ac.in>
2023-07-15 14:15:22 +0530

    index & VQA pages added :pushpin:

--- VQA/urls.py
+++ VQA/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("admin/", admin.site.urls),
+    path('',views.userlogin,name='userlogin'),
+    path('index/',views.index,name='index'),
+    path('register/',views.register,name='register'),
+    path('logoutUser/',views.logoutUser,name='logoutUser'),
+    path('vqa/',views.vqa,name='vqa'),
+
 ]
--- VQA/views.py
+++ VQA/views.py
@@ -1,6 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Registration, Question
+from django.contrib.auth.models import User
+from .utils.sample import answer_question
 
-# Create your views here.
 
 def index(request):
-    return render(request, 'VQA/index.html')
+    name = request.user.username.upper()
+    return render(request, 'VQA/index.html', {'name': name})
+
+
+def userlogin(request):
+    if request.user.is_authenticated:
+        name = request.user.username.upper()
+        return render(request, 'VQA/index.html', {'name': name})
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        curr_user = authenticate(username=username, password=password)
+        if curr_user is not None:
+            login(request, curr_user)
+            if curr_user.is_superuser:
+                return render(request, 'VQA/index.html', {'name': username.upper()})
+            else:
+                return redirect('index')  # Redirect to the desired page after successful login
+        else:
+            return render(request, 'VQA/login.html')
+    else:
+        return render(request, 'VQA/login.html')
+
+
+def logoutUser(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('userlogin')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        curr_val = Registration(username=username, password=password)
+        curr_val.save()
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        print(curr_val)
+        return render(request, 'VQA/register.html')
+    else:
+        return render(request, 'VQA/register.html')
+
+
+def vqa(request):
+    if request.method == 'POST':
+        questions = request.POST.get('questions')
+        image = request.FILES.get('image')
+        if image:
+            # Add image to the database
+            my_model = Question(question=questions)
+            my_model.image = image
+            my_model.save()
+
+            # Call the function from the other file
+            answer = answer_question(my_model.image.name, questions)
+            print(answer)  # Print the answer for testing
+            return render(request, 'VQA/vqa.html', {'name': request.user.username.upper(), 'answer': answer})
+
+    return render(request, 'VQA/vqa.html', {'name': request.user.username.upper()})
